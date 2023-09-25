3eff3634882b36d81381de3abe3ad5a34d71858d
Kevin Xu <kevinxu16@gmail.com>
2023-09-25 09:05:14 +0700

    WIP - update script to execute in order

--- processImages.js
+++ processImages.js
@@ -16,24 +16,83 @@ const path = require('path');
 const jimp = require('jimp');
 const convert = require('heic-convert');
 
+const INPUT_DIR = "./media/test";
+const OUTPUT_DIR = "./assets/test"; 
 
-async function resizeAndSaveJPG(filePath) {
+processImages(INPUT_DIR);
+
+async function processImages(inputFileDirectory) {
+    files = fs.readdirSync(inputFileDirectory);
+
+    for (var i = 0; i < files.length; i++) {
+        const file = files[i];
+        console.log("Processing File:", inputFileDirectory + "/" + file);
+
+        if (path.extname(file).toLowerCase() == ".heic") {
+            await processHEIC(inputFileDirectory + "/" + file);
+        } else if (path.extname(file).toLowerCase() == ".jpg") {
+            await processJPG(inputFileDirectory + "/" + file);
+        } else {
+            console.log("Skipping file:", file);
+        }
+        console.log("\n");
+    }
+}
+
+// convert HEIC --> JPG, resize JPG
+async function processHEIC(inputFilePath) {
+    console.log("convertHEIC()");
     try {
-        console.log("resizeJPG()", filePath);
-        const outputFilePath = `./assets/${filePath}`;
-    
-        const inputBuffer = fs.readFileSync(filePath);
-        const image = await jimp.read(inputBuffer);
+        // Read the HEIC file into memory
+        const inputBuffer = fs.readFileSync(inputFilePath);
+        const outputFilePath = getNewFilePath(getFileNameFromPath(inputFilePath));
+
+        const outputBuffer = await convert({
+            buffer: inputBuffer, // the HEIC file buffer
+            format: 'JPEG',      // output format
+            quality: 1          // the jpeg compression quality, between 0 and 1
+        });
+
+        const image = await jimp.read(outputBuffer);
         image
             .resize(1280, jimp.AUTO) // Resize the image to a width of 300 pixels
             .quality(80) // Set the quality to 80%
-            //.write(outputFilePath); // Save the image to a file 
-    
-        console.log(`Saving JPEG: ${outputFilePath}`);
+            .write(outputFilePath); // Save the image to a file 
+        console.log("Saving File:", outputFilePath);
+    } catch (error) {
+        console.error('Error in convertHEIC():', error);
+    }
+}
+
+async function processJPG(inputFilePath) {
+    console.log("resizeJPG()");
+    try {
+        const inputBuffer = fs.readFileSync(inputFilePath);
+        const outputFilePath = getNewFilePath(getFileNameFromPath(inputFilePath));
+
+        const image = await jimp.read(inputBuffer);
+        image
+            .resize(1560, jimp.AUTO) // Resize the image to a width of 300 pixels
+            .quality(80) // Set the quality to 80%
+            .write(outputFilePath); // Save the image to a file 
+        console.log(`Saving File: ${outputFilePath}`);
     } catch (error) {
         console.error('Error in resizeJPG():', error);
     }
 }
+
+function getFileNameFromPath(inputFilePath) {
+    return path.basename(inputFilePath);
+}
+function getNewFileName(fileName) {
+    return fileName.toLowerCase().split(".").shift() + ".jpg";
+}
+function getNewFilePath(fileName) {
+    return OUTPUT_DIR + "/" + getNewFileName(fileName);
+}
+
+
+/*
 
 async function convertHEIC(filePath) {
     console.log("convertHEIC()", filePath);
@@ -114,11 +173,8 @@ async function readFilesInDirectory(directoryPath) {
 }
   
 readFilesInDirectory('./media');
+*/
 
-
-//const inputFilePath = './media/IMG_3846.heic';
-//const outputFilePath = './media/IMG_3846.jpg';
-//convertHeicToJpeg();
 
 
 
