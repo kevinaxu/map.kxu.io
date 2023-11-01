657e56594c111aef87d3844e2588d2a091398da1
Kevin Xu <kevinxu16@gmail.com>
2023-11-01 20:08:42 +0400

    Use /map dir for processImages script

--- scripts/processImages.js
+++ scripts/processImages.js
@@ -17,8 +17,8 @@ const jimp = require('jimp');
 const convert = require('heic-convert');
 
 // TODO: update these to the correct directories whenever photos get added!
-const INPUT_DIR = "./media/singapore";
-const OUTPUT_DIR = "./assets/singapore"; 
+const INPUT_DIR = "./media/map";
+const OUTPUT_DIR = "./assets/map"; 
 processImages(INPUT_DIR, OUTPUT_DIR);
 
 async function processImages(inFileDir, outFileDir) {
